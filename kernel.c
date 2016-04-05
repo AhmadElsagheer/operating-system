470f928d777d5dd5dbe13e7c14931c035a66a477
AhmedSoliman7 <AhmedSoliman7@github.com>
2016-04-05 12:11:02 +0200

    everythin is working till delete

--- kernel.c
+++ kernel.c
@@ -7,6 +7,10 @@ void readSector(char*, int);
 void handleInterrupt21(int, int, int, int);
 int mod(int, int);
 int div(int, int);
+void executeProgram(char*, int);
+void terminateProgram();
+void writeSector(char*, int);
+void deleteFile(char*);
 
 int main() 
 {
@@ -48,11 +52,38 @@ int main()
 	// interrupt(0x21,0,line,0,0);
 	// interrupt(0x21,3,line,0,0);
 
-	char buffer[13312]; 
-	makeInterrupt21();
+	// M3 TASK 1
+	// makeInterrupt21();
+	// char buffer[13312]; 
 	
-	interrupt(0x21, 3, "messag\0", buffer, 0); /*read the file into buffer*/
-	interrupt(0x21, 0, buffer, 0, 0); /*print out the file*/
+	
+	// interrupt(0x21, 3, "messag\0", buffer, 0); /*read the file into buffer*/
+	// interrupt(0x21, 0, buffer, 0, 0); /*print out the file*/
+
+	// M3 TASK 2
+	// interrupt(0x21, 4, "tstprg\0", 0x2000, 0);
+
+	// M3 TASK 3
+	// interrupt(0x21, 4, "tstpr2\0", 0x2000, 0);	
+	
+	// M3 TASK 4
+	char buffer[13312];
+	int i;
+	makeInterrupt21();
+	// interrupt(0x21, 0, "Hello\0", 0, 0);
+	// interrupt(0x21, 0, buffer, 0, 0);
+	interrupt(0x21, 3, "messag\0", buffer, 0); // try to read messag
+	interrupt(0x21, 0, buffer, 0, 0); //print out the contents of buffer
+	interrupt(0x21, 7, "messag\0", 0, 0); //delete messag
+	// interrupt(0x21, 7, "messag\0", 0, 0); //delete messag
+	// interrupt(0x21, 7, "messag\0", 0, 0); //delete messag
+	// interrupt(0x21, 7, "messag\0", 0, 0); //delete messag
+	// interrupt(0x21, 7, "messag\0", 0, 0); //delete messag
+
+	for(i = 0; i < 13312; ++i)
+		buffer[i] = 0x00;
+	interrupt(0x21, 3, "messag\0", buffer, 0); // try to read messag
+	interrupt(0x21, 0, buffer, 0, 0); //print out the contents of buffer
 	
 	while(1); /*hang up*/
 
@@ -149,14 +180,80 @@ void readFile(char* fileName , char* buffer)
 			}
 		if(!match)
 			continue;
-			
+		interrupt(0x21, 0, "File Found\0", 0, 0); 
 		for(j = 6; j < 32; j++)
 			readSector(buffer + (j - 6)*512, directory[i*32 + j]);
 		
 		break;
 	}
+	interrupt(0x21, 0, "End of read\0", 0, 0); 
 
 }
+
+void executeProgram(char* name, int segment)
+{
+	char buffer[13312];
+	int i;
+	i = 0;
+	readFile(name, buffer);
+	while(i < 13312)
+	{
+		putInMemory(segment, i, buffer[i]);
+		i++;
+	}
+	launchProgram(segment);
+}
+
+
+void terminateProgram()
+{
+	while(1);	
+}
+
+void writeSector(char* buffer, int sector)
+{
+	int rSector = mod(sector,18) + 1;
+	int head = mod(div(sector,18),2);
+	int track = div(sector,36);
+	interrupt(0x13,3*256+1,buffer,track*256+rSector,head*256);
+}
+
+void deleteFile(char* name)
+{
+	char map[512];
+	char directory[512];
+	char clean[512];	
+
+	int i, j, match;
+	readSector(map, 1);
+	readSector(directory, 2);
+
+	for(i = 0; i < 512; ++i)
+		clean[i] = 0x00;
+	for ( i = 0; i < 16; i++)
+	{
+		match = 1;
+		for(j = 0; j < 6; j++)
+			if(directory[i*32 + j] != name[j])
+			{
+				match = 0;
+				break;
+			}
+		if(!match)
+			continue;
+		directory[i*32] = 0x00;
+		for(j = 6; j < 32; j++)
+		{
+			// writeSector(clean, directory[i*32+j]);
+			map[directory[i*32+j]+1] = 0x00;
+		}
+		
+		break;
+	}
+	writeSector(map, 1);
+	writeSector(directory, 2);
+}
+
 
 void handleInterrupt21(int ax, int bx, int cx, int dx)
 {
@@ -169,6 +266,14 @@ void handleInterrupt21(int ax, int bx, int cx, int dx)
 		readSector(bx, cx);
 	else if(ax == 3)
 		readFile(bx,cx);
+	else if(ax == 4)
+		executeProgram(bx, cx);
+	else if(ax == 5)
+		terminateProgram();
+	else if(ax == 6)
+		writeSector(bx, cx);
+	else if(ax == 7)
+		deleteFile(bx);
 	else
 		printString("an error message :3");
 }
